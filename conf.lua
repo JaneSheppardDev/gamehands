0bde4dd2f0c4c6f752d98aa279091b5de9917f10
Unknown <s013297g@student.staffs.ac.uk>
2017-10-19 11:47:43 +0100

    gamestates
    added states and buttons to change between them

--- conf.lua
+++ conf.lua
@@ -1,4 +1,5 @@
 function love.conf(t)
+  t.title = "GameHands"
 	t.window.width = 800
 	t.window.height = 600
 end
--- main.lua
+++ main.lua
@@ -1,11 +1,59 @@
 function love.load()
-  
+  gamestate = 0
 end
 
-function love.update()
-  
+function love.keyreleased(key)
+	if key == "return" and gamestate == 0 then
+    gamestate = 3
+    love.draw()
+	end
+  if key == "space" and gamestate == 0 then
+    gamestate = 4
+    love.draw()
+	end
+  if key == "escape" and gamestate == 0 then
+    love.event.quit()
+  end
+  if key == "escape" and (gamestate == 4 or gamestate == 3) then
+    gamestate = 0
+    love.draw()
+  end
+end
+
+function love.update(dt)
+  if gamestate == 0 then -- title screen
+    
+  end
+  if gamestate == 1 then -- game over screen
+    
+  end
+  if gamestate == 2 then -- shop screen
+    
+  end
+  if gamestate == 3 then -- game screen
+    
+  end
+  if gamestate == 4 then -- instructions screen
+    
+  end
 end
 
 function love.draw()
-  
+  if gamestate == 0 then
+    love.graphics.print("press ENTER to start the game", 400-105, 400+50)
+    love.graphics.print("press SPACE to view the instructions", 400-120, 400+75)
+    love.graphics.print("press ESCAPE to exit the game", 400-105, 400+100)
+  end
+  if gamestate == 1 then -- game over screen
+    
+  end
+  if gamestate == 2 then -- shop screen
+    
+  end
+  if gamestate == 3 then -- game screen
+    
+  end
+  if gamestate == 4 then -- instructions screen
+    love.graphics.print("press ESCAPE to go back", 400-80, 400+100)
+  end
 end
